e9785a9104c4e492ca0cf7ab0d7db8623f2f11ca
Artem Zakharchuk <artemzakharchuk@gmail.com>
2024-07-09 10:44:44 +0300

    solution

--- src/checkPassword.test.js
+++ src/checkPassword.test.js
@@ -1,19 +1,51 @@
-'use strict';
+"use strict";
 
 describe(`Function 'checkPassword':`, () => {
-  const checkPassword = require('./checkPassword');
+  const checkPassword = require("./checkPassword");
 
   it(`should be declared`, () => {
     expect(checkPassword).toBeInstanceOf(Function);
   });
 
   it(`should return boolean`, () => {
+    const isPasswordValid = checkPassword("P@ssword1");
 
+    expect(typeof isPasswordValid).toBe("boolean");
   });
 
   it(`should return 'true' for the valid password with 8 characters`, () => {
+    const isPasswordValid = checkPassword("P@ssword1");
 
+    expect(isPasswordValid).toBe(true);
   });
 
-  // write more tests here
+  it(`should return 'false' for the invalid password with 4 characters`, () => {
+    const isPasswordValid = checkPassword("P@d1");
+
+    expect(isPasswordValid).toBe(false);
+  });
+
+  it(`should return 'false' if password.length > 16`, () => {
+    const isPasswordValid = checkPassword("P@ssword1111111111111111111111111");
+
+    expect(isPasswordValid).toBe(false);
+  });
+
+  it(`should be case sensitive`, () => {
+    const isPasswordValid = checkPassword("p@ssword1");
+
+    expect(isPasswordValid).toBe(false);
+  });
+
+  it(`should have at least 1 special character`, () => {
+    const isPasswordValid = checkPassword("Password1");
+
+    expect(isPasswordValid).toBe(false);
+  });
+
+  it(`should have at least 1 digit`, () => {
+    const isPasswordValid = checkPassword("P@ssword");
+
+    expect(isPasswordValid).toBe(false);
+  });
 });
